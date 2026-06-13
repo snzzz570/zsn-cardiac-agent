7a70cd3d96ebdc003a61fd0e02d363dfc7ce3bb7
snzzz570 <yyy357771@gmail.com>
2026-06-13 12:04:45 +0800

    Delete src/CDS directory

--- src/CDS/train/custom/dataset/registry.py
+++
@@ -1,4 +0,0 @@
-# from mmcv.utils import Registry
-from mmengine.registry import Registry
-PIPELINES = Registry('pipelines')
-DATASETS = Registry('datasets')
--- src/CDS/train/custom/model/registry.py
+++
@@ -1,7 +0,0 @@
-# from mmcv.utils import Registry
-from mmengine.registry import Registry
-BACKBONES = Registry('backbone')
-NECKS = Registry('neck')
-HEADS = Registry('head')
-LOSSES = Registry('loss')
-NETWORKS = Registry('network')
